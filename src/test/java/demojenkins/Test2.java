acda48fce72f5b0ec79de751c95a06526f5a51de
nagar <nagar@Nagarajshetty>
2022-10-23 20:19:55 +0530

    hi

---
+++ src/test/java/demojenkins/Test2.java
@@ -0,0 +1,12 @@
+package demojenkins;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void tc2() {
+		Reporter.log("hii jenkins",true);
+	}
+
+}
